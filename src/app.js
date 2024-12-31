af6ec60b0570cf7caa612b25b20a1b55a83b1e50
HeMeher04 <hemsagarmeher786@gmail.com>
2024-12-31 23:32:01 +0530

    params and query

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const express = require("express")
 const app= express();
 
-app.use("/hello",(req,res)=>{
+app.get(/.*abye$/,(req,res)=>{
     res.send("Hello World ");
 })
 
+app.get("/user",(req,res)=>{
+    console.log(req.query);
+})
+
+app.get("/user/:uId/:name",(req,res)=>{
+    console.log(req.params);
+})
 app.listen(3000,()=>{
     console.log("Connected !");
 })
