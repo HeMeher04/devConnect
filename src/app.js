dd311df2d561c044a529e7b9e3f7e99d1bb9b659
HeMeher04 <hemsagarmeher786@gmail.com>
2025-01-14 19:25:14 +0530

    interacting with db get and post api call

--- src/app.js
+++ src/app.js
@@ -3,17 +3,52 @@ const express = require("express");
 const app = express();
 const User = require("./models/user.js");
 
-//Signup using hardcore
+//Signup taking dynamic data
+app.use(express.json());
 app.post("/signup",async(req,res)=>{
-    const newobj= new User({
-        name:"Hemsagar Meher",
-        email:"hemsagar@gmail.com",
-        age:27
-    })
-    await newobj.save();
-    res.send("User Added sucessfully")
+    const newobj= new User(req.body)
+    try{
+        await newobj.save();
+        res.send("User Added sucessfully");
+    }
+    catch(err){
+        res.send("Error",err);
+    }
+})
+// get data from database
+//matching data
+app.get("/matchingUser", async(req,res)=>{
+    try{
+        const user1 = await User.findOne({name:req.body.name});
+        if(user1.size==0){
+            res.send("No user with given name")
+        }
+        else{
+            console.log("Data sent")
+            res.send(user1);
+
+        }
+    }
+    catch(err){
+        res.status(404).send("Error in finding",err)
+    }
 })
 
+
+//find all user in database
+app.get("/feed", async(req,res)=>{
+    try{
+        const alluser = await User.find({}); //pass as empty obj
+        if(alluser.size==0){
+            res.send("No user with given name")
+        }
+        console.log("All user Data sent")
+        res.send(alluser);
+    }
+    catch(err){
+        res.status(404).send("Error in finding",err)
+    }
+})
 
 //connect to db using mongoose
 connectDB()
